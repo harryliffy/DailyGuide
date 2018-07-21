97f721004fa366388c020b7065c2c25e5be6058e
harryliffy <harryliffy@gmail.com>
2018-07-21 17:56:27 +0100

    January landing page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { JanuaryPage } from '../pages/january/january';
+
 import { FebruaryPage } from '../pages/february/february';
 import { ListPage } from '../pages/list/list';
 
---
+++ src/pages/january1/january1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { January1Page } from './january1';
+
+@NgModule({
+  declarations: [
+    January1Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(January1Page),
+  ],
+})
+export class January1PageModule {}
